9d4430ac325e8de7f6c692d387d8cb14c377feb3
v-nemeth <91267701+v-nemeth@users.noreply.github.com>
2023-02-15 11:07:23 +0100

    GMLAB-01 not working asteroids module

---
+++ AsteroidsEntityFramework/Asteroid/src/main/java/dk.sdu.mmmi.cbse.asteroidsystem/AsteroidControlSystem.java
@@ -0,0 +1,15 @@
+package dk.sdu.mmmi.cbse.asteroidsystem;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class AsteroidControlSystem implements IEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+    }
+
+
+}
