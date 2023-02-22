935a4a135fd2874dfbb664ebd628a9f829eaef9d
v-nemeth <viand21@student.sdu.dk>
2023-02-22 18:00:44 +0100

    bullet shape

--- AsteroidsEntityFramework/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
+++ AsteroidsEntityFramework/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -22,6 +22,7 @@ public class BulletControlSystem implements IEntityProcessingService {
             PositionPart positionPart = bullet.getPart(PositionPart.class);
             MovingPart movingPart = bullet.getPart(MovingPart.class);
 
+            movingPart.setUp(true);
             
             movingPart.process(gameData, bullet);
             positionPart.process(gameData, bullet);
@@ -38,17 +39,17 @@ public class BulletControlSystem implements IEntityProcessingService {
         float y = positionPart.getY();
         float radians = positionPart.getRadians();
 
-        shapex[0] = (float) (x + Math.cos(radians) * 20);
-        shapey[0] = (float) (y + Math.sin(radians) * 20);
+        shapex[0] = (float) (x + Math.cos(radians) * 5);
+        shapey[0] = (float) (y + Math.sin(radians) * 5);
 
-        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 20);
-        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 20);
+        shapex[1] = (float) (x + Math.cos(radians - 5 * 3.1415f / 5) * 5);
+        shapey[1] = (float) (y + Math.sin(radians - 5 * 3.1145f / 5) * 5);
 
         shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
         shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
 
-        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 20);
-        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 20);
+        shapex[3] = (float) (x + Math.cos(radians + 5 * 3.1415f / 5) * 5);
+        shapey[3] = (float) (y + Math.sin(radians + 5 * 3.1415f / 5) * 5);
 
         entity.setShapeX(shapex);
         entity.setShapeY(shapey);
