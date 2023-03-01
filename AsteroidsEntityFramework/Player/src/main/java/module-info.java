6aafa36d699c22a9d296594d123712a7e1269578
v-nemeth <viand21@student.sdu.dk>
2023-03-01 10:00:13 +0100

    module-info player

---
+++ AsteroidsEntityFramework/Player/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+//module-info.java
+module Player {
+    exports dk.sdu.mmmi.cbse.playersystem;
+
+    requires Common;
+    requires CommonPlayer;
+
+    provides dk.sdu.mmmi.cbse.common.services.IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
+    provides dk.sdu.mmmi.cbse.common.services.IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
+}
