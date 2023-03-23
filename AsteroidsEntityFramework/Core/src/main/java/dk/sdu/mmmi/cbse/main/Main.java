40567b47e272aec9b953dbc83030ea187c7031f1
v-nemeth <viand21@student.sdu.dk>
2023-03-23 10:10:36 +0100

    maven install working

--- AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("got here");
 
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Asteroids");
