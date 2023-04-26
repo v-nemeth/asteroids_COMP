c26adebcb954b806d613534ea3eb5011be7ab58a
v-nemeth <viand21@student.sdu.dk>
2023-04-26 09:58:29 +0200

    CollisionTest

---
+++ AsteroidsEntityFramework/CollisionController/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionControllerTest.java
@@ -0,0 +1,54 @@
+package dk.sdu.mmmi.cbse.collisionsystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CollisionControllerTest {
+    private Entity entity;
+    private PositionPart entityPositionPart;
+    private Entity entity2;
+    private PositionPart entity2PositionPart;
+    private CollisionController instance;
+
+    public CollisionControllerTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        instance = new CollisionController();
+        entity = new Entity();
+        entity2 = new Entity();
+
+        entityPositionPart = new PositionPart(0,0,0);
+        entity2PositionPart = new PositionPart(0,0,0);
+
+        entity.add(entityPositionPart);
+        entity2.add(entity2PositionPart);
+
+        entity.setRadius(10);
+        entity2.setRadius(10);
+    }
+
+    @Test
+    public void Collision() {
+        entityPositionPart.setPosition(0,0);
+        entity2PositionPart.setPosition(0,0);
+
+        Boolean expResult = true;
+        Boolean result = instance.collides(entity, entity2);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void NoCollision() {
+        entityPositionPart.setPosition(0,0);
+        entity2PositionPart.setPosition(20,20);
+
+        Boolean expResult = false;
+        Boolean result = instance.collides(entity, entity2);
+        assertEquals(expResult, result);
+    }
+}
--- AsteroidsEntityFramework/Core/src/main/java/module-info.java
+++ AsteroidsEntityFramework/Core/src/main/java/module-info.java
@@ -5,8 +5,11 @@ module Core {
     requires spring.context;
     requires spring.core;
     requires spring.beans;
+
     exports dk.sdu.mmmi.cbse.main;
+
     opens dk.sdu.mmmi.cbse.main to spring.core;
+
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
